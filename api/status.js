6a6643a7f10331ab33a96e7c9367d4ab5deaa3ad
tianyue <leoli@leo2.local>
2026-06-11 09:31:19 +0800

    fix: vercel rewrite exclude api/ routes

--- api/status.js
+++
@@ -1,5 +0,0 @@
-// GET /api/status - 简易诊断
-module.exports = (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('WECHAT_PAY_APPID=' + (process.env.WECHAT_PAY_APPID || 'NOT_SET'))
-}
